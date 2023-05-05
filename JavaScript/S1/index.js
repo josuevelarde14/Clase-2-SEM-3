e81e1debaa568a7052172c9be8fb4cb72afb8297
Josue Velarde <josuvelarde.14@gmail.com>
2023-05-04 20:05:36 -0500

    Avance

---
+++ JavaScript/S1/index.js
@@ -0,0 +1,19 @@
+//comentarios
+/*Comentarios en varias lineas
+varias lineas*/
+var nombre = "Josue";
+let edad = 24;
+//const PI = 3.14;
+
+let esMayor = true;
+let listaNumeros = [1, 2, 3, 4, 5];
+let objetoPersona = { nombre: "Josue", edad: 24};
+let valorNulo = null;
+let valorIndefinido = undefined;
+
+let a = 10;
+let b = 5;
+let resultado = a + b; // suma
+let esIgual = a == b; // Comparación: esIgual es igual a false
+let esVerdad = (a > 5) && (b < 10); // Operadores lógicos: esVerdad es igual a true
+
