b72c27f41bc2ffc909cef70bde01f8144b3a2822
Josue Velarde <josuvelarde.14@gmail.com>
2023-05-04 20:58:01 -0500

    Avance

---
+++ JavaScript/S2/S2 Ejercicios/index.js
@@ -0,0 +1,27 @@
+//comentarios
+/*Comentarios en varias lineas
+varias lineas*/
+var nombre = "Josue"; // cadena de texto
+let edad = 24; // numero
+//const PI = 3.14;
+
+let esMayor = true; // booleano
+let listaNumeros = [1, 2, 3, 4, 5]; // array
+let objetoPersona = { nombre: "Josue", edad: 24}; // objeto
+let valorNulo = null; // valor nulo
+let valorIndefinido = undefined; // valor indefinido
+
+let num1 = 10;
+let num2 = 5;
+let resultado = num1 + num2; // Suma: resultado es igual a 15
+let esIgual = num1 == num2; // Comparación: esIgual es igual a false
+let esVerdad = (num1 > 5) && (num2 < 10); // Operadores lógicos: esVerdad es igual a true
+
+
+let total=0,numeros = [1, 2, 3, 4, 5],a=0;
+numeros.forEach(function(a){total += a;});
+//console.log(total);
+
+let cadena1 = "hola";
+let cadena2 = "mundo";
+let cadenaConcatenada = cadena1 + ' ' + cadena2;
---
+++ JavaScript/S2/S2 Teoria/index.js
@@ -0,0 +1,18 @@
+//comentarios
+/*Comentarios en varias lineas
+varias lineas*/
+var nombre = "Josue"; // cadena de texto
+let edad = 24; // numero
+//const PI = 3.14;
+
+let esMayor = true; // booleano
+let listaNumeros = [1, 2, 3, 4, 5]; // array
+let objetoPersona = { nombre: "Josue", edad: 24}; // objeto
+let valorNulo = null; // valor nulo
+let valorIndefinido = undefined; // valor indefinido
+
+let a = 10;
+let b = 5;
+let resultado = a + b; // Suma: resultado es igual a 15
+let esIgual = a == b; // Comparación: esIgual es igual a false
+let esVerdad = (a > 5) && (b < 10); // Operadores lógicos: esVerdad es igual a true
