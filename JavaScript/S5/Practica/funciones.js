e971fd8ce73cc9e7892176f457c4bc58728071d0
Josue Velarde <josuvelarde.14@gmail.com>
2023-05-11 19:50:49 -0500

    avance2

---
+++ JavaScript/S5/Practica/funciones.js
@@ -0,0 +1,7 @@
+let miboton= document.getElementById("miboton");
+
+console.log(miboton);
+
+miboton.addEventListener("click",function(){alert("Me presionaste");}
+);
+
---
+++ JavaScript/S5/Teoria/Ej1/funciones.js
@@ -0,0 +1,9 @@
+let miboton= document.getElementById("miboton");
+
+console.log(miboton);
+
+miboton.addEventListener("click",function(){alert("Me presionaste");}
+);
+
+// # es para id
+// var button= $("miboton")
